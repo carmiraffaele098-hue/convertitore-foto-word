1fe9d839469e84e1f3e163e7620d5d6f2dd9a002
carmiraffaele098-hue <carmiraffaele098@gmail.com>
2026-08-16 11:10:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 API_KEY = os.getenv("GOOGLE_API_KEY")
 if API_KEY:
     genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.5-flash')
 
 HTML_PAGE = """
 <!DOCTYPE html>
